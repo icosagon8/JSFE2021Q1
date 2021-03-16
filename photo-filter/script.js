672d1ee480c83ad9c9274a94d23fe1c58d486236
icosagon8 <dmpavlov8@yandex.ru>
2021-03-16 23:05:14 +0300

    feat: add a change to the output value when moving the input

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -3,6 +3,10 @@
 function onFiltersInput(evt) {
   const suffix = evt.target.dataset.sizing;
   document.documentElement.style.setProperty(`--${evt.target.name}`, evt.target.value + suffix);
+
+  if (evt.target.nextElementSibling.matches('output[name=result]')) {
+    evt.target.nextElementSibling.value = evt.target.value;
+  }
 }
 
 const filters = document.querySelector('.filters');
