5cf81083476899e4f49225fad7a093bcf3dc1434
icosagon8 <dmpavlov8@yandex.ru>
2021-05-19 19:46:15 +0300

    feat: add main-nav component

---
+++ match-match-game/src/components/main-nav/main-nav.ts
@@ -0,0 +1,21 @@
+import './main-nav.scss';
+import { BaseComponent } from '../base-component';
+
+export class MainNav extends BaseComponent {
+  constructor() {
+    super('nav', ['main-nav']);
+    this.element.innerHTML = `
+      <ul class='main-nav__list'>
+        <li>
+          <a class='main-nav__link main-nav__link--about' href='#/about/'>About Game</a>
+        </li>
+        <li>
+          <a class='main-nav__link main-nav__link--score' href='#/score/'>Best Score</a>
+        </li>
+        <li>
+          <a class='main-nav__link main-nav__link--settings' href='#/settings/'>Game Settings</a>
+        </li>
+      </ul>
+      `;
+  }
+}
