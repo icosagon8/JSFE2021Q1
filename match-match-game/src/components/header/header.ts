7d7165cc3aa555aac2192f650e668caaaa675f8f
icosagon8 <dmpavlov8@yandex.ru>
2021-05-19 19:52:17 +0300

    fix: correct header component

--- match-match-game/src/components/header/header.ts
+++ match-match-game/src/components/header/header.ts
@@ -1,11 +1,37 @@
 import './header.scss';
 import { BaseComponent } from '../base-component';
+import { Container } from '../container/container';
+import { Logo } from '../logo/logo';
+import { MainNav } from '../main-nav/main-nav';
+import { Button } from '../button/button';
+import { Avatar } from '../avatar/avatar';
 
 export class Header extends BaseComponent {
+  private readonly container: Container;
+
+  private readonly logo: Logo;
+
+  private readonly nav: MainNav;
+
+  private readonly button: Button;
+
+  private readonly avatar: Avatar;
+
   constructor() {
     super('header', ['header']);
-    this.element.innerHTML = `
-      <div class='header__wrapper'></div>
-    `;
+    this.container = new Container();
+    this.container.element.classList.add('header__wrapper');
+    this.element.appendChild(this.container.element);
+    this.logo = new Logo();
+    this.logo.element.classList.add('header__logo');
+    this.container.element.appendChild(this.logo.element);
+    this.nav = new MainNav();
+    this.nav.element.classList.add('header__nav');
+    this.container.element.appendChild(this.nav.element);
+    this.button = new Button();
+    this.button.element.classList.add('header__btn');
+    this.container.element.appendChild(this.button.element);
+    this.avatar = new Avatar();
+    this.container.element.appendChild(this.avatar.element);
   }
 }
