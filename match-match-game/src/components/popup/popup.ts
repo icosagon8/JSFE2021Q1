0ada51a0289b18cd96d440858b0aa6b9132031e3
icosagon8 <dmpavlov8@yandex.ru>
2021-05-25 13:10:46 +0300

    feat: add popup methods

--- match-match-game/src/components/popup/popup.ts
+++ match-match-game/src/components/popup/popup.ts
@@ -9,4 +9,12 @@ export class Popup extends Component {
     super(parentNode, 'div', ['popup-overlay']);
     this.popup = new Component(this.element, 'div', [`${name}-popup`, 'popup']);
   }
+
+  showPopup(): void {
+    this.popup.element.parentElement?.classList.add('popup-overlay--active');
+  }
+
+  closePopup(): void {
+    this.popup.element.parentElement?.classList.remove('popup-overlay--active');
+  }
 }
