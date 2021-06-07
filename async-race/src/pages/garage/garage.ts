0e6794c753a8a75be73538ad8476d8057e7b80ce
icosagon8 <dmpavlov8@yandex.ru>
2021-06-07 15:19:14 +0300

    feat: add Garage page

---
+++ async-race/src/pages/garage/garage.ts
@@ -0,0 +1,77 @@
+import './garage.scss';
+import { Component } from '../../components/component';
+import { RootElement } from '../../models/root-element-model';
+
+export class Garage extends Component {
+  title: Component;
+
+  pageCount: Component;
+
+  Controls: Component;
+
+  carCreateControls: Component;
+
+  carNameCreateInput: Component;
+
+  carColorCreateInput: Component;
+
+  carCreateBtn: Component;
+
+  carUpdateControls: Component;
+
+  carNameUpdateInput: Component;
+
+  carColorUpdateInput: Component;
+
+  carUpdateButton: Component;
+
+  raceControls: Component;
+
+  raceBtn: Component;
+
+  resetBtn: Component;
+
+  generateBtn: Component;
+
+  constructor(parentNode: RootElement) {
+    super(parentNode, 'div', ['page', 'garage']);
+    this.title = new Component(this.element, 'h1', ['garage__title'], 'Garage (4)');
+    this.pageCount = new Component(this.element, 'p', ['garage__page-count'], 'Page №1');
+    this.Controls = new Component(this.element, 'div', ['garage__controls']);
+    this.carCreateControls = new Component(this.Controls.element, 'div', ['garage__group']);
+    this.carNameCreateInput = new Component(this.carCreateControls.element, 'input', ['garage__name-input'], '', [
+      ['type', 'text'],
+      ['name', 'name'],
+      ['autocomplete', 'off'],
+    ]);
+    this.carColorCreateInput = new Component(this.carCreateControls.element, 'input', ['garage__color-input'], '', [
+      ['type', 'color'],
+      ['name', 'color'],
+      ['autocomplete', 'off'],
+    ]);
+    this.carCreateBtn = new Component(this.carCreateControls.element, 'button', ['btn', 'garage__btn'], 'Create');
+
+    this.carUpdateControls = new Component(this.Controls.element, 'div', ['garage__group']);
+    this.carNameUpdateInput = new Component(this.carUpdateControls.element, 'input', ['garage__name-input'], '', [
+      ['type', 'text'],
+      ['name', 'name'],
+      ['autocomplete', 'off'],
+    ]);
+    this.carColorUpdateInput = new Component(this.carUpdateControls.element, 'input', ['garage__color-input'], '', [
+      ['type', 'color'],
+      ['name', 'color'],
+      ['autocomplete', 'off'],
+    ]);
+    this.carUpdateButton = new Component(this.carUpdateControls.element, 'button', ['btn', 'garage__btn'], 'Update');
+
+    this.raceControls = new Component(this.Controls.element, 'div', ['garage__group']);
+    this.raceBtn = new Component(this.raceControls.element, 'button', ['btn', 'garage__btn'], 'Race');
+    this.resetBtn = new Component(this.raceControls.element, 'button', ['btn', 'garage__btn'], 'Reset');
+    this.generateBtn = new Component(
+      this.raceControls.element,
+      'button',
+      ['btn', 'garage__btn', 'garage__btn--generate'],
+      'Generate cars'
+    );
+  }
+}
