cd274d58fd9ba53bae45cc49f21c1e57047c6f8d
icosagon8 <dmpavlov8@yandex.ru>
2021-06-07 18:33:49 +0300

    feat: add page number and number of cars from the store to garage page

--- async-race/src/pages/garage/garage.ts
+++ async-race/src/pages/garage/garage.ts
@@ -1,6 +1,7 @@
 import './garage.scss';
 import { Component } from '../../components/component';
 import { RootElement } from '../../models/root-element-model';
+import { store } from '../../store';
 
 export class Garage extends Component {
   title: Component;
@@ -35,8 +36,8 @@ export class Garage extends Component {
 
   constructor(parentNode: RootElement) {
     super(parentNode, 'div', ['page', 'garage']);
-    this.title = new Component(this.element, 'h1', ['garage__title'], 'Garage (4)');
-    this.pageCount = new Component(this.element, 'p', ['garage__page-count'], 'Page №1');
+    this.title = new Component(this.element, 'h1', ['garage__title'], `Garage (${store.carsNumber})`);
+    this.pageCount = new Component(this.element, 'p', ['garage__page-count'], `Page №${store.page}`);
     this.Controls = new Component(this.element, 'div', ['garage__controls']);
     this.carCreateControls = new Component(this.Controls.element, 'div', ['garage__group']);
     this.carNameCreateInput = new Component(this.carCreateControls.element, 'input', ['garage__name-input'], '', [
@@ -50,7 +51,6 @@ export class Garage extends Component {
       ['autocomplete', 'off'],
     ]);
     this.carCreateBtn = new Component(this.carCreateControls.element, 'button', ['btn', 'garage__btn'], 'Create');
-
     this.carUpdateControls = new Component(this.Controls.element, 'div', ['garage__group']);
     this.carNameUpdateInput = new Component(this.carUpdateControls.element, 'input', ['garage__name-input'], '', [
       ['type', 'text'],
@@ -63,7 +63,6 @@ export class Garage extends Component {
       ['autocomplete', 'off'],
     ]);
     this.carUpdateButton = new Component(this.carUpdateControls.element, 'button', ['btn', 'garage__btn'], 'Update');
-
     this.raceControls = new Component(this.Controls.element, 'div', ['garage__group']);
     this.raceBtn = new Component(this.raceControls.element, 'button', ['btn', 'garage__btn'], 'Race');
     this.resetBtn = new Component(this.raceControls.element, 'button', ['btn', 'garage__btn'], 'Reset');
