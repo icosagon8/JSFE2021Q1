d6a7bdb6fc0b639a48c9b620570750f9296dc3a2
icosagon8 <dmpavlov8@yandex.ru>
2021-06-13 18:02:46 +0300

    feat: change Store interface

--- async-race/src/models/store-model.ts
+++ async-race/src/models/store-model.ts
@@ -1,10 +1,14 @@
 import { CarModel } from './car-model';
 import { RequestFrame } from './request-frame-model';
+import { WinnerModel } from './winner-model';
 
 export interface StoreModel {
   cars: CarModel[];
   carsNumber: number;
-  page: number;
+  garagePage: number;
+  winners: WinnerModel[];
+  winnersNumber: number;
+  winnersPage: number;
   selectedCar: CarModel | null;
   carsRequestId: Record<string, RequestFrame>;
 }
