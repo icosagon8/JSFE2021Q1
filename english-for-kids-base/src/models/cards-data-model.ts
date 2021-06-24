62d909d7d9542e54b95211745805afaa134e7491
icosagon8 <dmpavlov8@yandex.ru>
2021-06-24 21:23:53 +0300

    feat: add cards data

---
+++ english-for-kids-base/src/models/cards-data-model.ts
@@ -0,0 +1,6 @@
+import { WordDataModel } from './word-data-model';
+
+export interface CardsDataModel {
+  category: string;
+  words: WordDataModel[];
+}
---
+++ english-for-kids-base/src/models/word-data-model.ts
@@ -0,0 +1,6 @@
+export interface WordDataModel {
+  word: string;
+  translation: string;
+  image: string;
+  audioSrc: string;
+}
