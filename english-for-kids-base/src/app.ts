0d59b3e40f9205eb6b94769b012b3a4ccd67d78d
icosagon8 <dmpavlov8@yandex.ru>
2021-06-25 19:00:24 +0300

    feat: display the main page

--- english-for-kids-base/src/app.ts
+++ english-for-kids-base/src/app.ts
@@ -1,10 +1,14 @@
 import { Component } from './components/component';
 import { Header } from './components/header/header';
+import { MainPage } from './pages/main';
 
 export class App {
   private readonly header: Component;
 
+  private readonly page: MainPage;
+
   constructor(private readonly rootElement: HTMLElement) {
     this.header = new Header(this.rootElement);
+    this.page = new MainPage(this.rootElement);
   }
 }
