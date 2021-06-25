1d98c90a0e6567437a923b5e661d2b84f91a4d1b
icosagon8 <dmpavlov8@yandex.ru>
2021-06-25 20:33:22 +0300

    feat: add footer to the app

--- english-for-kids-base/src/app.ts
+++ english-for-kids-base/src/app.ts
@@ -1,14 +1,18 @@
 import { Component } from './components/component';
 import { Header } from './components/header/header';
 import { MainPage } from './pages/main';
+import { Footer } from './components/footer/footer';
 
 export class App {
   private readonly header: Component;
 
   private readonly page: MainPage;
 
+  private readonly footer: Footer;
+
   constructor(private readonly rootElement: HTMLElement) {
     this.header = new Header(this.rootElement);
     this.page = new MainPage(this.rootElement);
+    this.footer = new Footer(this.rootElement);
   }
 }
