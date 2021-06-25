e9199db1c7c6c28e1e13c7f8845a54d760bc45af
icosagon8 <dmpavlov8@yandex.ru>
2021-06-25 18:58:45 +0300

    feat: add Main page

---
+++ english-for-kids-base/src/pages/main.ts
@@ -0,0 +1,20 @@
+import { Component } from '../components/component';
+import { RootElement } from '../models/root-element-model';
+import { CardsField } from '../components/cards-field/cards-field';
+import { getCategoryData } from '../services/cards-services';
+import { CardCategory } from '../components/card-category/card-category';
+
+export class MainPage extends Component {
+  cardsField: CardsField;
+
+  constructor(parentNode: RootElement) {
+    super(parentNode, 'main', ['container', 'main']);
+    this.cardsField = new CardsField(this.element, 'categories');
+    this.addCategoryCards();
+  }
+
+  addCategoryCards(): void {
+    const categoryData = getCategoryData();
+    categoryData.forEach((category) => new CardCategory(this.cardsField.container.element, category));
+  }
+}
