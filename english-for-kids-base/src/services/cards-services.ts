46d1123c037d013c8d98a391bfceeb46263ce4cc
icosagon8 <dmpavlov8@yandex.ru>
2021-06-26 23:59:31 +0300

    feat: add cards service method for getting the category names

--- english-for-kids-base/src/services/cards-services.ts
+++ english-for-kids-base/src/services/cards-services.ts
@@ -4,3 +4,5 @@ import { CategoryDataModel } from '../models/category-data-model';
 export const getCategoryData = (): CategoryDataModel[] => {
   return cards.map((item) => item.category);
 };
+
+export const getCategoryNames = (): string[] => cards.map((item) => item.category.name);
