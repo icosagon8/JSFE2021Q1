893f9826c17e937e528fcec515e83777769f073f
icosagon8 <dmpavlov8@yandex.ru>
2021-06-28 23:22:17 +0300

    feat: implement method for adding cards to the cardfield

--- english-for-kids-base/src/pages/category.ts
+++ english-for-kids-base/src/pages/category.ts
@@ -1,6 +1,9 @@
+import { CardWord } from '../components/card-word/card-word';
 import { CardsField } from '../components/cards-field/cards-field';
 import { Component } from '../components/component';
 import { RootElement } from '../models/root-element-model';
+import { getWordsData } from '../services/cards-services';
+import { store } from '../store/store';
 
 export class Category extends Component {
   cardsField: CardsField;
@@ -8,5 +11,12 @@ export class Category extends Component {
   constructor(parentNode: RootElement) {
     super(parentNode, 'main', ['container', 'main']);
     this.cardsField = new CardsField(this.element, 'words');
+    this.addCategoryCards();
+  }
+
+  addCategoryCards(): void {
+    const category = store.getState().page;
+    const wordsData = getWordsData(category);
+    wordsData.forEach((wordData) => new CardWord(this.cardsField.container.element, wordData));
   }
 }
