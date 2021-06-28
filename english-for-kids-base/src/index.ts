b48ef83f201e0c9afddb030ee8d7b36f751eeb37
icosagon8 <dmpavlov8@yandex.ru>
2021-06-28 23:23:45 +0300

    feat: implement redirect to the Main page when updating the page

--- english-for-kids-base/src/index.ts
+++ english-for-kids-base/src/index.ts
@@ -2,4 +2,7 @@ import './assets/styles/styles.scss';
 import { App } from './app';
 
 const rootNode = document.body;
-window.onload = () => new App(rootNode);
+window.onload = () => {
+  window.location.hash = '#/';
+  (() => new App(rootNode))();
+};
