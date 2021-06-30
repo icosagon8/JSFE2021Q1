685e2f2bcc4b2380529377dbc6152c55d9f505cf
icosagon8 <dmpavlov8@yandex.ru>
2021-06-30 23:32:02 +0300

    feat: implement the start/repeat button behavior

--- english-for-kids-base/src/helpers/utils.ts
+++ english-for-kids-base/src/helpers/utils.ts
@@ -1,3 +1,18 @@
 export const getKebabCaseString = (string: string): string => {
   return string.replace(/\s/g, '-').toLocaleLowerCase();
 };
+
+export const shuffleArray = <T>(array: T[]): T[] => {
+  let j;
+  let temp;
+  const arr = [...array];
+
+  for (let i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+
+  return arr;
+};
--- english-for-kids-base/src/pages/category/category.ts
+++ english-for-kids-base/src/pages/category/category.ts
@@ -1,24 +1,39 @@
 import './category.scss';
+import { Unsubscribe } from 'redux';
 import { CardWord } from '../../components/card-word/card-word';
 import { CardsField } from '../../components/cards-field/cards-field';
 import { Component } from '../../components/component';
 import { RootElement } from '../../models/root-element-model';
 import { getWordsData } from '../../services/cards-services';
 import { store } from '../../store/store';
+import { shuffleArray } from '../../helpers/utils';
 
 export class Category extends Component {
   cardsField: CardsField;
 
-  startBtn: Component;
+  button: Component;
 
   cards: CardWord[];
+
+  shuffleCards?: CardWord[];
+
+  currentCard?: CardWord;
+
+  unsubscribe: Unsubscribe;
 
   constructor(parentNode: RootElement) {
     super(parentNode, 'main', ['container', 'main']);
     this.cardsField = new CardsField(this.element, 'words');
-    this.startBtn = new Component(this.cardsField.element, 'button', ['category__start-btn'], 'Start');
+    this.button = new Component(this.cardsField.element, 'button', ['category__btn--start'], 'Start');
     this.cards = [];
     this.addCategoryCards();
+    this.unsubscribe = store.subscribe(this.controlButtonState);
+    this.setEventHandlers();
+  }
+
+  private setEventHandlers(): void {
+    this.button.element.addEventListener('click', this.startGame, { once: true });
+    window.addEventListener('hashchange', () => this.unsubscribe(), { once: true });
   }
 
   addCategoryCards(): void {
@@ -29,4 +44,27 @@ export class Category extends Component {
       this.cards.push(card);
     });
   }
+
+  repeatAudio = (): void => {
+    this.currentCard?.playAudio();
+  };
+
+  startGame = (): void => {
+    this.shuffleCards = shuffleArray<CardWord>(this.cards);
+    this.currentCard = this.shuffleCards.pop();
+    this.currentCard?.playAudio();
+    this.button.element.classList.add('category__btn--repeat');
+    this.button.element.addEventListener('click', this.repeatAudio);
+  };
+
+  private controlButtonState = (): void => {
+    const { isPlayMode } = store.getState().mode;
+
+    if (isPlayMode) {
+      this.button.element.classList.remove('category__btn--repeat');
+      this.button.element.addEventListener('click', this.startGame, { once: true });
+    } else {
+      this.button.element.removeEventListener('click', this.repeatAudio);
+    }
+  };
 }
