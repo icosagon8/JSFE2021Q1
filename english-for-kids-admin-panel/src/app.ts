42fee039db141ac2efafb5619d4ffbf9f68d286e
icosagon8 <dmpavlov8@yandex.ru>
2021-07-07 13:52:30 +0300

    feat: add the main file of the express application

---
+++ english-for-kids-admin-panel/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { categoryRouter } from './routes/categoryRouter';
+
+const PORT = 3000;
+
+async function start() {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://icosagon8:rssefk@cluster0.lnrdj.mongodb.net/efk?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
+    const app = express();
+    app.use(express.json());
+    app.use('/categories', categoryRouter);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
